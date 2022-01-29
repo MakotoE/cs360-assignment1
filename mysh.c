3cc9f325bc182b305a792afaa84f4c647afe52eb
Makoto <makoto@emurasoft.com>
2022-01-28 16:20:07 -0800

    chdir

--- mysh.c
+++ mysh.c
@@ -1,3 +1,22 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+char* chdirerror(int err) {
+	switch (err) {
+	case EACCES:
+		return "access denied";
+	case ENOENT:
+		return "path does not exist";
+	case ENOTDIR:
+		return "path is not a directory";
+	default:
+		return "unknown error";
+	}
+}
+
 int main(int argc, char* argv[]) {
-	return 0;
+	if (chdir("")) {
+		printf("%s", chdirerror(errno));
+	}
 }
