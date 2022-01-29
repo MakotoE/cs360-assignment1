e1c7afc3dc26d9e43dbeded06726af226e452c45
Makoto <makoto@emurasoft.com>
2022-01-28 16:44:02 -0800

    getcwd

--- mysh.c
+++ mysh.c
@@ -1,22 +1,15 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
-
-char* chdirerror(int err) {
-	switch (err) {
-	case EACCES:
-		return "access denied";
-	case ENOENT:
-		return "path does not exist";
-	case ENOTDIR:
-		return "path is not a directory";
-	default:
-		return "unknown error";
-	}
-}
+#include <string.h>
+#include <linux/limits.h>
 
 int main(int argc, char* argv[]) {
 	if (chdir("")) {
-		printf("%s", chdirerror(errno));
+		printf("%s", strerror(errno));
 	}
+
+	char cwd[PATH_MAX];
+	getcwd(cwd, sizeof(cwd));
+	printf("%s", cwd);
 }
