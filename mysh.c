0fbe25cca3ac71ac31d51c8a8445d5392eef7af7
Makoto <makoto@emurasoft.com>
2022-01-30 13:01:03 -0800

    comments

--- mysh.c
+++ mysh.c
@@ -1,3 +1,5 @@
+// mysh: A shell that can run commands.
+
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,10 +10,12 @@
 #include <sys/wait.h>
 #include <pwd.h>
 
+// Returns true if s has given prefix.
 bool hasprefix(const char *s, const char *prefix) {
 	return strncmp(s, prefix, strlen(prefix)) == 0;
 }
 
+// Removes trailing newline if s ends with newline.
 void removenewline(char *s) {
 	size_t len = strlen(s);
 	if (*(s + len - 1) == '\n') {
@@ -19,6 +23,8 @@ void removenewline(char *s) {
 	}
 }
 
+// Splits s by space character and returns the resulting array of strings. The s argument gets
+// modified.
 char **parseargs(char *s) {
 	char **args = calloc(sizeof(char *), 1);
 	if (args == NULL) {
@@ -49,6 +55,7 @@ char **parseargs(char *s) {
 	return args;
 }
 
+// Frees the array of strings.
 void freeargs(char **args) {
 	for (size_t i = 0; args[i] != NULL; ++i) {
 		free(args[i]);
@@ -56,6 +63,7 @@ void freeargs(char **args) {
 	free(args);
 }
 
+// Calls command for given args, where args[0] is the file name.
 void callcommand(char *const *args) {
 	assert(args != NULL);
 	assert(args[0] != NULL);
@@ -77,6 +85,7 @@ void callcommand(char *const *args) {
 	}
 }
 
+// Replaces home directory with '~' and writes replaced directory to out.
 void translatepath(const char path[static PATH_MAX], char out[static PATH_MAX]) {
 	char *homedir = getpwuid(getuid())->pw_dir;
 	if (hasprefix(path, homedir)) {
@@ -93,10 +102,10 @@ const char *exitcommand = "exit";
 
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[]) {
 	while (true) {
-		char cwd[PATH_MAX];
+		char cwd[PATH_MAX] = {0};
 		getcwd(cwd, sizeof(cwd));
 
-		char directorywithreplacement[PATH_MAX];
+		char directorywithreplacement[PATH_MAX] = {0};
 		translatepath(cwd, directorywithreplacement);
 
 		printf("%s$mysh> ", directorywithreplacement);
